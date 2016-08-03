042bee8e962a2eeb2789ff6a93cc2e6bb2a536a7
Bart C <bart.chr@gmail.com>
2016-08-02 22:30:15 -0400

    Add animation chain
    consists of commented timeout animation functions to cycle through the different words. I may come back to this on a later date to allow someone to easily manipulate the different time durations and text fields and not have to go to each one.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,37 @@
 // scripts for site
+$(document).ready(function () {
+    setTimeout(function () {
+        textAnimate();
+    }, 700);
+});
+
+
+function textAnimate() {
+    setTimeout(function () {
+        $("#textChange").addClass("fadeOutUp");
+    }, 800);
+    setTimeout(function () {
+        $("#textChange").removeClass("fadeOutUp"); //#1
+    }, 1200); //+0.8seconds
+    setTimeout(function () {
+        $("#textChange").addClass("fadeInUp").text("HARDWORKING."); //#2 TEXT CHANGE
+    }, 1201);
+    setTimeout(function () {
+        $("#textChange").removeClass("fadeInUp").addClass("fadeOutUp"); //#3
+    }, 2600); //+1.4seconds
+    setTimeout(function () {
+        $("#textChange").removeClass("fadeOutUp"); //#1
+    }, 3400); //+0.8seconds
+    setTimeout(function () {
+        $("#textChange").addClass("fadeInUp").text("GREGARIOUS."); //#2 TEXT CHANGE
+    }, 3401);
+    setTimeout(function () {
+        $("#textChange").removeClass("fadeInUp").addClass("fadeOutUp"); //#3
+    }, 4800); //+1.4seconds
+    setTimeout(function () {
+        $("#textChange").removeClass("fadeOutUp"); //#1
+    }, 5600); //+0.8seconds
+    setTimeout(function () {
+        $("#textChange").addClass("fadeInUp").text("CLASS OF 2021."); //#2 TEXT CHANGE
+    }, 5601);
+}
